69a374787f2a8528a4db9531375262286d5ed050
Kendall Wahnschaffe <kpwahnschaffe@gmail.com>
2017-05-19 17:26:07 -0500

    Basic routing

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,17 +6,22 @@ import Leaderboard from './leaderboard';
 import AddTeam from './add_team';
 import AddPlayer from './add_player';
 
+// Reset all default styles
+import '../../style/reset.css';
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Toolbar/>
         <BrowserRouter>
-          <Switch>
-            <Route path="/addTeam" component={AddTeam} />
-            <Route path="/addPlayer" component={AddPlayer} />
-            <Route path="/" component={Leaderboard} />
-          </Switch>
+            <div>
+                <Toolbar/>
+                <Switch>
+                    <Route path="/addPlayer" component={AddPlayer} />
+                    <Route path="/addTeam" component={AddTeam} />
+                    <Route path="/" component={Leaderboard} />
+                </Switch>
+            </div>
         </BrowserRouter>
       </div>
     );
--- frontend/src/components/toolbar.js
+++ frontend/src/components/toolbar.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import '../../style/toolbar.css'
 
 class Toolbar extends Component {
     render() {
         return (
-            <div>
-                <h1>Ping Pong</h1>
-                <div></div>
+            <div className="nav">
+                <h1 className="nav-titles"><Link to="/" className="nav-title"> Ping Pong </Link></h1>
+                <div className="nav-links">
+                    <Link to="/addPlayer" className="nav-link"> Add Player </Link>
+                    <Link to="/addTeam" className="nav-link"> Add Team </Link>
+                </div>
             </div>
         )
     }
