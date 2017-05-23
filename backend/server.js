26ce86cdf6799c3bdba7084809585fa3894d6ba3
jonathanfilsaime <jonathanfilsaime@gmail.com>
2017-05-23 17:29:28 -0500

    updates

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var readSingles = require('./db_readSingles')
 var readDoubles = require('./db_readDoubles')
+var test = require('./test')
 var app = express();
 
 app.get('/', function (req, res)
@@ -22,5 +23,10 @@ app.get('/readDoubles', function(req, res)
         readDoubles(function (result){res.send(result)});
     }
 )
+
+app.get('/test/:name', function(req, res)
+{
+    test(function (callback){res.send(callback)} , req.params.name);
+})
 
 app.listen(3000);
---
+++ backend/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by jonathanfilsaime on 5/23/2017.
+ */
+module.exports = function read(callback, name)
+{
+    callback(name);
+}
