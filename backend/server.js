4cccc9335c99f7cf7a714b9b222f793ec91932b7
Kendall Wahnschaffe <kpwahnschaffe@gmail.com>
2017-06-02 09:21:43 -0500

    amazon stuff

--- backend/server.js
+++ backend/server.js
@@ -53,6 +53,10 @@ app.get('/readAllDoubles', function(req, res) {
     });
 });
 
+app.get('/', function(req, res) {
+    res.sendfile('/public/index.html', { root: __dirname });
+});
+
 http.createServer(app).listen(8080	, function () {
     console.log("Listening on port 8080");
 });
